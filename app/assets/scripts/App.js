c553d618c6a3c1a9e7b825279fb79701f14a4d84
the_Iceman <kushgarakumar34@gmail.com>
2019-02-07 01:56:04 +0530

    Finished the toggling of the mobile menu with the help of javaScript and Jquery. Now styling is left

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,16 +1,3 @@
-//ES6 way of importing 
-import Person from './modules/Person';
+import MobileMenu from './modules/MobileMenu'
 
-class Adult extends Person {
-  patTaxes() {
-    console.log(this.name + " have zero tax");
-  }
-}
-alert("Testing 123");
-
-var kush = new Person("Kushagra", "blue");
-kush.greet();
-
-var rimmi = new Adult("Anamika", "ornage");
-rimmi.greet();
-rimmi.patTaxes();
+var mobileMenu = new MobileMenu();
